03d5bac61f501363d0a2a5b71ef353f00524b566
Vitor A. Rinkawetsky <88173262+NerdskyX@users.noreply.github.com>
2023-03-07 15:06:27 -0300

    fgh

--- projeto01/src/app/paginas/novo-componente/novo-componente.component.ts
+++ projeto01/src/app/paginas/novo-componente/novo-componente.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./novo-componente.component.css']
 })
 export class NovoComponenteComponent {
+  //atributos
+  private nome:string;
+  private anoNascimento:number;
+
+  //contrutor
+  constructor() {
+    this.nome = "Vitor Belo Horizonte"
+    this.anoNascimento = 2005
+  }
+
+  //ações
+
+  idade(): number {
+    return 2023 - this.anoNascimento;
+  }
+
+  ngOnInit(): void{
+    console.log("Nome: " + this.nome)
+    console.log("Idade: "+ this.idade())
+  }
 
 }
